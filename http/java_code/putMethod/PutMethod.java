ee4e4476866e215fa465391649a1de3c48136564
rishithSadashiv <rishith.1si16ec078@gmail.com>
2019-08-06 15:30:41 +0530

    java_code

---
+++ http/java_code/putMethod/PutMethod.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class PutMethod {
+
+	public static void main(String[] args) throws IOException {
+		URL url ;
+		String a;		
+		try {
+			url= new URL("http://imdhanush.herokuapp.com/dcn");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("PUT");
+			//System.out.println(con.getInputStream());
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+			while((a = in.readLine()) != null)
+			{
+				System.out.println(a);
+			}
+			in.close();	
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
